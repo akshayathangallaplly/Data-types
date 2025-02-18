db999af4ca127fe4c4c2291ac1b54eaadbe6c333
akshayathangallapally <ta7440360@gmail.com>
2025-02-18 22:43:58 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,40 @@
+'''
+int
+boolean
+float
+complex
+'''
+a=1
+b=-22
+print(type(a),type(b))
+
+#boolean
+a=True
+b=False
+print(type(a),type(b))
+
+#float
+c=4.44
+print(type(c))
+
+#complex
+v=2+5j
+print(type(v))
+
+'''
+int()
+float()
+bool()
+str()
+'''
+# convert int to float
+a=2
+print(float(a))
+
+# convert float to int
+b=5.5
+print(int(b))
+
+'''
+data lose-->explicit type conversion
+no data lose--->implicit type conversion'''
